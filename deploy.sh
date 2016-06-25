9b60fefa43630bfc066d47ed953ff67038e46762
colt05-usr <a677acf1@opayq.com>
2016-06-25 13:15:11 -0400

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -8,12 +8,16 @@ SHA=`git rev-parse --verify HEAD`
 # -other stuff would go here, but I am just pushing to the Repo-
 
 git config user.name "Travis CI" >> /dev/null
+echo set name
 git config user.email "$COMMIT_AUTHOR_EMAIL" >> /dev/null
+echo set email
 
 # Commit the "changes", i.e. the new version.
 # The delta will show diffs between new and old versions.
 git add bitlisten.min.js
+echo added file
 git commit -m "Deploy to GitHub Pages: ${SHA}" >> /dev/null
+echo added commit
 
 # Get the deploy key by using Travis's stored variables to decrypt deploy_key.enc
 ENCRYPTED_KEY_VAR="encrypted_${ENCRYPTION_LABEL}_key"
@@ -24,6 +28,8 @@ openssl aes-256-cbc -K $ENCRYPTED_KEY -iv $ENCRYPTED_IV -in deploy_key.enc -out 
 chmod 600 deploy_key
 eval `ssh-agent -s`
 ssh-add deploy_key >> /dev/null
+echo added ssh key
 
 # Now that we're all set up, we can push.
 git push $SSH_REPO gh-pages >> /dev/null
+echo pushed
