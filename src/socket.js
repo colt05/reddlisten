fe8ca4f532f45242e36758efe2f68ea83b2dd70f
colt05-usr <colt05-usr@users.noreply.github.com>
2016-06-25 14:47:18 -0400

    Update socket.js

--- src/socket.js
+++ src/socket.js
@@ -20,7 +20,7 @@ TransactionSocket.init = function() {
 		eventToListenTo = 'tx';
     		room = 'inv';
 
-    		var socket = io("//mainnet.decred.org/");
+    		var socket = io("https://".concat(provider_name).concat("/");
     		socket.on('connect', function() {
 			 // Join the room.
       			socket.emit('subscribe', room);
